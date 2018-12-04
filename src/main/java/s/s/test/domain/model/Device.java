65cc21a04db30c985f831274c6b5d9cdf78efbd4
Mainframe\Vlad <vzinchenko36@gmail.com>
2018-12-04 22:33:12 +0300

    1. Given class Device. 2. Added class User.

---
+++ src/main/java/s/s/test/domain/model/Device.java
@@ -0,0 +1,15 @@
+package s.s.test.domain.model;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Device {
+
+    private UUID id;
+    private String serialNo;
+    private String type;
+    private String description;
+    private User owner;
+    private Date createdAt;
+
+}

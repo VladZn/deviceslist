08cead895006829f8ff84d3d51330a0abd9608c5
Mainframe\Vlad <vzinchenko36@gmail.com>
2018-12-07 15:35:41 +0300

    Getters and Setter, constructor added

--- src/main/java/s/s/test/domain/model/Device.java
+++ src/main/java/s/s/test/domain/model/Device.java
@@ -10,8 +10,12 @@ import javax.validation.constraints.Size;
 import java.util.Date;
 import java.util.UUID;
 
+@SuppressWarnings("JpaQlInspection")
+@NamedQueries({@NamedQuery(name = Device.GET_ALL, query = "SELECT d FROM Device d WHERE d.owner.id=:userId ORDER BY d.createdAt DESC"),})
 @Table(name = "devices", uniqueConstraints = {@UniqueConstraint(columnNames = "serial_no", name = "devices_serial_no_uindex")})
 public class Device {
+
+    public static final String GET_ALL = "Device.getAll";
 
     @Id
     private UUID id;
@@ -44,5 +48,60 @@ public class Device {
     public Device() {
     }
 
+    public Device(@NotBlank @Size(min = 4, max = 50) String serialNo, @NotBlank @Size(min = 2, max = 50) String type, @NotBlank @Size(min = 2, max = 255) String description, @NotNull User owner) {
+        this.id = UUID.randomUUID();
+        this.serialNo = serialNo;
+        this.type = type;
+        this.description = description;
+        this.owner = owner;
+        this.createdAt = new Date();
+    }
 
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getSerialNo() {
+        return serialNo;
+    }
+
+    public void setSerialNo(String serialNo) {
+        this.serialNo = serialNo;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
 }
